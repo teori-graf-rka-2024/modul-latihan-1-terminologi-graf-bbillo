d2ae0b995784c6320adac05882402e9b5ed8a4a0
bbillo <168956414+bbillo@users.noreply.github.com>
2025-03-18 23:34:49 +0700

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -1,42 +1,29 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 #1
-def create_graph(edges: list[tuple[int, int]]) -> nx.Graph:
+def create_graph(edges:list[tuple[int, int]]) -> nx.Graph:
     G = nx.Graph()
     G.add_edges_from(edges)
     return G
-edges = [(1,2), (2,3)]
-G = create_graph(edges)
-print(G)
 
 #2
 def get_degree(G: nx.Graph, node: int) -> int:
-    degrees = G.degree(node)
-    return degrees
-nodes = 2
-print(get_degree(G, nodes))
+    return G.degree[node]
 
 #3
-def traversal(G: nx.Graph, start: int) -> list[int]:
-    dfs = list(nx.dfs_preorder_nodes(G, source=start))
-    return dfs
-print(traversal(G, 1))
+def dfs_traversal(G: nx.Graph, start: int) -> list[int]:
+    return list(nx.dfs_preorder_nodes(G, start))
 
 #4
-def traversal(G: nx.Graph, start: int) -> list[int]:
-    bfs = list(nx.bfs_tree(G, source=start))
-    return bfs
-print(traversal(G, 1))
+def bfs_traversal(G: nx.Graph, start: int) -> list[int]:
+    return list(nx.bfs_tree(G, source=start))
 
 #5
 def find_shortest_path(G: nx.Graph, source: int, target: int) -> list[int]:
-    shortest = nx.shortest_path(G, source=source, target=target)
-    return shortest
-print(find_shortest_path(G, 1, 3))
+    return nx.shortest_path(G, source=source, target=target)
 
 #6
-import matplotlib.pyplot as plt
 def visualize_graph(G: nx.Graph) -> None:
     nx.draw(G)
-    plt.savefig('graf.png')
-print(visualize_graph(G))
+    plt.show()
